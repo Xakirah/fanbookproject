7ad613fc545d67eee11013c70dcb1b71a97f8e71
Xakirah <harisdwirahmsnyh23@gmail.com>
2024-05-21 10:17:06 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://harisd:harisqwe23@cluster0.keidjvf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-db = client.dbsparta
+MONGODB_URL = os.environ.get("MONGODB_URL")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URL)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -25,6 +34,7 @@ def homework_post():
 @app.route("/homework", methods=["GET"])
 def homework_get():
     message_list = list(db.fanmessages.find({}, {'_id': False}))
+    print(f"Message List: {message_list}")  # Debug log
     return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
